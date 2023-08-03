886f77904e2c199bbf97d8c145e66fe883d922fe
Adiination <Aditya.sasaram11@gmail.com>
2023-08-03 13:07:01 +0530

    timer component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Timer from "./components/Timer";
 
 function App() {
+  const handleTimerEnd = () => {
+    alert("time is up!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>timer</h1>
+
+      <Timer time={30} direction="clockwise" end={handleTimerEnd} />
+      <Timer time={20} direction="anticlockwise" end={handleTimerEnd} />
     </div>
   );
 }
---
+++ src/components/Timer.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+const Timer = ({ time, direction, end }) => {
+  const [remainingTime, setRemainingTime] = useState(0);
+  useEffect(() => {
+    let intervalId;
+    if (direction === "clockwise") {
+      setRemainingTime(0);
+      intervalId = setInterval(() => {
+        setRemainingTime((prevTime) =>
+          prevTime + 1 > time ? time : prevTime + 1
+        );
+      }, 1000);
+    } else if (direction === "anticlockwise") {
+      setRemainingTime(time);
+      intervalId = setInterval(() => {
+        setRemainingTime((prevTime) => (prevTime - 1 < 0 ? 0 : prevTime - 1));
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [direction, time]);
+  useEffect(() => {
+    if (remainingTime === time || remainingTime === 0) {
+      end();
+    }
+  }, [remainingTime, time, end]);
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = seconds % 60;
+    return `${minutes.toString().padStart(2, "0")}:${secondsLeft
+      .toString()
+      .padStart(2, "0")}`;
+  };
+  return <div>{formatTime(remainingTime)}</div>;
+};
+
+export default Timer;
