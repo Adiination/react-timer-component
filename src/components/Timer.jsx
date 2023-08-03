c936fe9a8d8ac63754f4149813c561541bcbe82c
Adiination <Aditya.sasaram11@gmail.com>
2023-08-03 18:25:37 +0530

    fix: alert was coming before the timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 
 const Timer = ({ time, direction, end }) => {
   const [remainingTime, setRemainingTime] = useState(0);
+  const [hasStarted, setHasStarted] = useState(false);
   useEffect(() => {
     let intervalId;
     if (direction === "clockwise") {
       setRemainingTime(0);
       intervalId = setInterval(() => {
+        setHasStarted(true);
         setRemainingTime((prevTime) =>
           prevTime + 1 > time ? time : prevTime + 1
         );
@@ -14,16 +16,17 @@ const Timer = ({ time, direction, end }) => {
     } else if (direction === "anticlockwise") {
       setRemainingTime(time);
       intervalId = setInterval(() => {
+        setHasStarted(true);
         setRemainingTime((prevTime) => (prevTime - 1 < 0 ? 0 : prevTime - 1));
       }, 1000);
     }
     return () => clearInterval(intervalId);
   }, [direction, time]);
   useEffect(() => {
-    if (remainingTime === time || remainingTime === 0) {
+    if (hasStarted && (remainingTime === time || remainingTime === 0)) {
       end();
     }
-  }, [remainingTime, time, end]);
+  }, [remainingTime, time, end, hasStarted]);
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const secondsLeft = seconds % 60;
